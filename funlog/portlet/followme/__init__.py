16503465023d961f149230afcd2caa4735dd4002
andy fang <andy@mingtak.com.tw>
2015-03-23 14:33:20 +0000

    initial version

---
+++ funlog/portlet/followme/__init__.py
@@ -0,0 +1,6 @@
+from zope.i18nmessageid import MessageFactory
+FollowMeMessageFactory = MessageFactory('funlog.portlet.followme')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
